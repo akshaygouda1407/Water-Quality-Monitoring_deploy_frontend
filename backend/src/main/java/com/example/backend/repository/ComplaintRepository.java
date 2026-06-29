6bf763c9bb7ef0896214281af7632df7871652f1
Akshay Gouda <akshaygouda1407@gmail.com>
2026-06-29 12:57:20 +0530

    added

--- backend/src/main/java/com/example/backend/repository/ComplaintRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Complaint;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
-}
